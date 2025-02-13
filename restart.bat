94a14832f3f4db0e58e68f4b6e1200e37bf4e589
itlboy <nkien.bk@gmail.com>
2025-02-13 17:51:27 +0700

    update

---
+++ restart.bat
@@ -0,0 +1,10 @@
+@echo off
+cd /d %~dp0
+echo Stop if running
+docker compose down -t0
+echo Pulling latest images...
+docker compose pull
+echo Starting containers...
+docker compose up -d
+echo Done.
+docker compose logs -f
