1af2a44692ec9c28ebdffd0cb515d30ea1f3ac8e
itlboy <nkien.bk@gmail.com>
2025-02-21 10:10:57 +0700

    update

--- docker-status.bat
+++ docker-status.bat
@@ -1,9 +1,9 @@
 @echo off
 docker info >nul 2>&1
 IF %ERRORLEVEL% NEQ 0 (
-    echo Docker is NOT running.
+    echo Docker is NOT running. Restarting the computer in 10 seconds...
     timeout /t 10 /nobreak >nul
-    exit /b 1
+    shutdown /r /t 0
 ) ELSE (
     echo Docker is running.
     timeout /t 10 /nobreak >nul
